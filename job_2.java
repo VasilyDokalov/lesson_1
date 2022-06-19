72796f7759e6803d204b4459831d24ff2fef36bd
hamster <dokalov@outlook.com>
2022-06-20 00:52:02 +0300

    modified job_2

--- job_2.java
+++ job_2.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-
-import javax.security.sasl.SaslException;
 public class job_2 {
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
